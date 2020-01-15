f02c0f184b6b74e654f242610d3df054bbbdba79
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-01-15 17:53:36 +0900

    section 6 commit

---
+++ clear Java section6/calcapp/CalcLogic.java
@@ -0,0 +1,12 @@
+package calcapp;
+public class CalcLogic{
+    
+    public static int add(int a, int b){
+    return (a + b);
+    }
+
+    public static int compare(int a, int b) {
+        return (a-b);
+    }
+
+}
