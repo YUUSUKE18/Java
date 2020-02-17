18883ec0f77937667a7dbd3b972b07b55ad56feb
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-02-17 16:03:56 +0900

    Java Array Study

--- App/Game.java
+++ App/Game.java
@@ -1,15 +1,15 @@
-class Game {
+public class Game {
     public static void main(String args[]){
         System.out.println("数当てゲーム");
-        int ans = new java.util.Random().nextInt(9);
-        for(int i=0; i < 5; i++){
-            System.out.println("0から9の数字を入力してください");
-            int num = new java.util.Scanner(System.in).nextInt();
-            if(ans == num){
+            int [] numbers = {3, 4, 9};
+            System.out.println("一桁の数字を入力してください");
+            int input = new java.util.Scanner(System.in).nextInt();
+            for(int n : numbers){
+                if(input == n){
                 System.out.println("あたり！");
-            } else {
-                System.out.println("外れ");
-            }
+                } else {
+                    System.out.println("外れ");
+                }
         }
         System.out.println("ゲームを終了します");
     }
