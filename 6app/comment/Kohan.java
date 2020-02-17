459c4c9442ec9a6ecf35365fe031a817fd9dd73c
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-02-18 00:28:51 +0900

    Section6 Done

---
+++ 6app/comment/Kohan.java
@@ -0,0 +1,12 @@
+
+public class Kohan {
+    public static void callDeae(){
+        System.out.println("ええい、曲者め、出会えい！");
+    }
+
+    public static void showMondokoro() throws Exception {
+        System.out.println("ここで良いでしょう");
+        System.out.println("この紋所が目に入らぬか");
+        doTogame();
+    }
+}
